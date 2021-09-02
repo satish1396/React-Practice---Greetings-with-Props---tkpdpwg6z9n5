a931b6270c3801ad9bd0ec2a01201a4abbd2805f
satish1396 <88325876+satish1396@users.noreply.github.com>
2021-09-02 12:22:40 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,13 @@
-// write code for Welcome component here
+// write code for Welcome component hereimport React from "react";
+import '../styles/App.css';
+import Welcome from './Welcome';
+
+const App = () => {
+  return (
+    <Welcome />
+  )
+}
+
+
+export default App;
+
