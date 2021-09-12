5aecdf2501b901983f810785383e5ef25d5f29d3
satish1396 <88325876+satish1396@users.noreply.github.com>
2021-09-12 13:04:53 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,7 +5,7 @@ function Welcome(props) {
   return (
      <>
       <h1>Hey {props.name}!</h1>
-      <h2>{"Welcome to Newton School."}</h2>  
+      <h2>"Welcome to Newton School."</h2>  
     </>
 }
 
